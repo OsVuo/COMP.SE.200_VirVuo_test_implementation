1fd08753d20976a8c928772ef2f209d23141f0dc
RikuVa <120555800+RikuVa@users.noreply.github.com>
2022-12-16 12:35:50 +0200

    Update differenceUnitTest.js

--- test/differenceUnitTest.js
+++ test/differenceUnitTest.js
@@ -15,10 +15,10 @@ var myExpect = expect.expect;
 describe('expect', function(){
     
     it('should exclude the values in the second array from the first array', function(){
-        myExpect(ceil([2,1], [2,3])).to.equal([1]);
+        myExpect(difference([2,1], [2,3])).to.equal([1]);
     });
 
     it('should return an empty array if both arrays are identical', function(){
-        myExpect(ceil([2,1], [2,1])).to.equal([]);
+        myExpect(difference([2,1], [2,1])).to.equal([]);
     });
 });
