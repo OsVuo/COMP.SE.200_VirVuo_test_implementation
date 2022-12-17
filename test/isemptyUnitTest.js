d22a65c5946cb5da7e5f213e156e564fa577bed8
RikuVa <120555800+RikuVa@users.noreply.github.com>
2022-12-17 03:23:08 +0200

    Add files via upload

---
+++ test/isemptyUnitTest.js
@@ -0,0 +1,19 @@
+/**
+ * Simple unit test where function isempty is tested.
+ * 
+ * function should return true or false depending on whether the object that it examines is empty
+ */
+
+import expect from "chai";
+
+import isEmpty from '../application/isEmpty';
+
+var myExpect = expect.expect;
+
+describe('isEmpty', function(){
+    
+    it('should return false if null is entered', function(){
+        myExpect(isEmpty(null)).deep.to.eq(false);
+    });
+
+});
