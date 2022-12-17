19d9e1f179240968153cbdd8e65bc4cea5bd39bd
RikuVa <120555800+RikuVa@users.noreply.github.com>
2022-12-17 04:14:00 +0200

    Update filterUnitTest.js

--- test/filterUnitTest.js
+++ test/filterUnitTest.js
@@ -13,7 +13,26 @@ var myExpect = expect.expect;
 describe('filter', function(){
     
     it('should filter Arrays correctly', function(){
-        const users = [{ 'user': 'Hawkeye', 'active': true },{ 'user': 'Radar', 'active': false }]
-        myExpect(filter(users, ({ active }) => active)).to.deep.eq([{ user: 'Hawkeye', active: true }]);
+        const users = [{ 'user': 'Hawkeye', 'active': true },{ 'user': 'Frank', 'active': false },
+        { 'user': 'Radar', 'active': true }]
+        myExpect(filter(users, ({ active }) => active)).to.deep.eq([{ 'user': 'Hawkeye', 'active': true },{ 'user': 'Radar', 'active': true }]);
     });
+    it('should not filter anything with a non matching filter', function(){
+        const users = [{ 'user': 'Scooby', 'Scaredy-Cat': true },{ 'user': 'Shaggy', 'Hungry': true },
+        { 'user': 'Velma', '20-20': false }, { 'user': 'Fred', 'ascot': true }]
+        myExpect(filter(users, ({ active }) => active)).to.deep.eq(users);
+    });
+    it('should return an empty array if the filter filters everything', function(){
+        const users = [{ 'user': 'Leonardo', 'Pizza': true },{ 'user': 'Raphael', 'Pizza': true },
+        { 'user': 'Donatello', 'Pizza': true }, { 'user': 'Michelangelo', 'Pizza': true }]
+        myExpect(filter(users, ({ Pizza }) => Pizza)).to.deep.eq(users);
+    });
+    it('should throw an error when given two illegal arguments', function(){
+        const illegalArgumentsAdd = () => filter('a', 'b');
+        myExpect(illegalArgumentsAdd).to.throw();
+    });
+    it('should throw an error when given no arguments', function(){
+        const noArgumentsAdd = () => filter();
+        myExpect(noArgumentsAdd).to.throw();
+    }); 
 });
