43611d5e0aa633636b3ca172d00cb61a37caa2b7
RikuVa <120555800+RikuVa@users.noreply.github.com>
2022-12-17 03:32:22 +0200

    Update isemptyUnitTest.js

--- test/isemptyUnitTest.js
+++ test/isemptyUnitTest.js
@@ -16,4 +16,29 @@ describe('isEmpty', function(){
         myExpect(isEmpty(null)).to.eq(true);
     });
 
+    it('should return true if true is entered', function(){
+        myExpect(isEmpty(true)).to.eq(true);
+    });    
+    it('should return true if a single integer is entered', function(){
+        myExpect(isEmpty(1)).to.eq(true);
+    });
+    it('should return false if a non empty array is entered', function(){
+        myExpect(isEmpty([1,2,3])).to.eq(false);
+    });
+    it('should return true if an empty array is entered', function(){
+        myExpect(isEmpty([])).to.eq(true);
+    });
+    it('should return false if a non empty map is entered', function(){
+        myExpect(isEmpty({'a': 1})).to.eq(false);
+    });
+    it('should return true if an empty map is entered', function(){
+        myExpect(isEmpty({})).to.eq(true);
+    });   
+    it('should return false if a string is entered', function(){
+        myExpect(isEmpty("abc kissa kavelee")).to.eq(false);
+    });  
+    it('should return true if an empty string is entered', function(){
+        myExpect(isEmpty("")).to.eq(true);
+    });  
+
 });
